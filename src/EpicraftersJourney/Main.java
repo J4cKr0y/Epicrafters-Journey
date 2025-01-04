b38fd183bf4e1bf3b43cc80cec1e3f1fec5969c7
J4cKr0y <Kiky270281@gmail.com>
2025-01-05 00:06:57 +0100

    First commit

---
+++ src/EpicraftersJourney/Main.java
@@ -0,0 +1,23 @@
+package EpicraftersJourney;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int num;
+		num=5;
+		num=10;
+		final int immuable=20;
+
+		String langage="Java";
+		String phrase=langage + " est un langage de programmation.";
+		
+		System.out.println(phrase);
+		
+		int[] unTableauDEntier = {5,10,15,20};
+		int num2dutableau;
+		num2dutableau= unTableauDEntier[1];
+		System.out.println(num2dutableau);
+	}
+
+}
