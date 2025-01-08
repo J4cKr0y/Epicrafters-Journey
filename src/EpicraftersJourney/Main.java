d3e73ce4fd4eca971bba24c76ee92bb4fea58b3b
J4cKr0y <Kiky270281@gmail.com>
2025-01-08 23:35:50 +0100

    Feat: Tableaux et structure itérative

--- src/EpicraftersJourney/Main.java
+++ src/EpicraftersJourney/Main.java
@@ -1,9 +1,12 @@
 package EpicraftersJourney;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//variables
+//variables
 		int num;
 		num=5;
 		num=10;
@@ -12,16 +15,26 @@ public class Main {
 		String langage="Java";
 		String phrase=langage + " est un langage de programmation.";
 		System.out.println(phrase);
-		//tableau
+//tableau
 		int[] unTableauDEntier = {5,10,15,20};
 		int num2dutableau;
 		num2dutableau= unTableauDEntier[1];
 		System.out.println(num2dutableau);
-		//fonctions
+//fonctions
 		int somme = Main.addition(2,7);
 		System.out.println(somme);
 		decompte(5);
 		affichage("Marty");
+		commentLaMeteo("soleil");
+		String film1="La petite sirène";
+		String film2="Indiana Jones";
+		System.out.println("La catégorie de "+ film1 +" est : " + categorieDeFilm(film1));
+		System.out.println("La catégorie de "+ film2 +" est : " + categorieDeFilm(film2));
+		afficherLangage2Prog();
+		
+		int[] chiffreDontOnDoitCompterLesZero = {26, 10, 1985, 0, 12, 11, 1955};
+		combienYaDeZero(chiffreDontOnDoitCompterLesZero);
+		
 	}
 
 	public static int addition(final int entier1, final int entier2) {
@@ -39,5 +52,51 @@ public class Main {
 		final String salutation="Bonjour ";
 		String phrase=salutation+nom1;
 		System.out.println(phrase);
+	}
+	
+//	structure itérative switch
+	public static void commentLaMeteo(final String meteo) {
+		switch(meteo) {
+		case "soleil" -> System.out.println("Beau temps");
+        case "nuage" -> System.out.println("Couvert");
+        case "pluie" -> System.out.println("Mauvais temps");
+        default -> System.out.println("Je ne sais pas.");
+		}
+	}
+	
+	public static String categorieDeFilm(final String film) {
+	    var resultat = switch (film) {
+	        case "Star Wars" -> "Science fiction";
+	        case "Blanche neige", "La petite sirène" -> "Disney";
+	        case "Indiana Jones" -> {
+	            String categorie = "Aventure";
+	            yield categorie;
+//Si les instructions sont dans un bloc { } le mot-clé yield doit être utilisé.
+	        }
+	        default -> "Inconnu";
+	    };
+	    return resultat;
+	}
+	
+//	structure itérative for
+	public static void afficherLangage2Prog() {
+	    List<String> nomsDesLangages = Arrays.asList("Java", "PHP", "JavaScript");
+	    for (String nomDUnLangage : nomsDesLangages) {
+	        System.out.println(nomDUnLangage);
+	    }
+	}
+	
+	public static void combienYaDeZero(final int[] tableau) { 
+		int cpt = 0;
+//Bouclez sur chaque case.
+		for (int chiffre : tableau) {
+//Si la valeur est égale à 0, incrémentez une variable nommée cpt. 
+	        if (chiffre == 0)
+	        	{
+	        	cpt++;
+	        	}
+	    }
+//Après l’exécution de la boucle, afficher la variable cpt.
+	System.out.println(cpt);
 	}
 }
